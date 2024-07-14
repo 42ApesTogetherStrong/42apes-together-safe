ceb71e1f5ab65ff240b77d9ab634c63aa84aa0e7
Ender13120 <117198798+Ender13120@users.noreply.github.com>
2024-07-14 02:53:02 +0200

    fix: typescript issues

--- components/ScheduledTransferForm.tsx
+++ components/ScheduledTransferForm.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+/* eslint-disable */
+// @ts-ignore
 import { execHaloCmdWeb } from "@arx-research/libhalo/api/web";
 
 import { SafeSmartAccountClient } from "@/lib/permissionless";
